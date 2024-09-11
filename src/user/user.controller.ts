5a3f45b356e477469bae5b0e4a1c16583e1eb17f
Fidel <fidellegra60@gmail.com>
2024-09-11 16:16:58 -0400

    feat(user): added updateUser function

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,27 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Param, ParseIntPipe, Put, Res, UseGuards } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { Response } from 'express';
+import { AdminGuard } from 'src/auth/admin.guard';
 
 @ApiTags('user')
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ) {}
+
+    @Put('/:id')
+    @UseGuards(AdminGuard)
+    @ApiResponse({status: 239, description: 'updated user info successfully'})
+    @ApiResponse({status: 452, description: 'error updating the user info'})
+    @ApiResponse({status: 454, description: 'user not found'})
+    updateUser(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updateUser: UpdateUserDto,
+        @Res() res: Response
+    ) {
+        return this.userService.updateUser(id, updateUser, res);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { UserModel } from './model/user.model';
+import { Response } from 'express';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        private readonly userModel: UserModel
+    ) {}
+
+    async updateUser(
+        id: number,
+        {email, phone, username, name, lastName}: UpdateUserDto,
+        res: Response
+    ) {
+        try {
+            const userDb: User = await this.userModel.getSecureUserById(id);
+
+            if(!userDb) return res.sendStatus(424);
+
+            await this.userModel.updateUser(id, {email, phone, username, name, lastName});
+
+            return res.sendStatus(239);
+        } catch (error) {
+            this.handleLog('updateUser', error);
+            return res.sendStatus(452);
+        }
+    }
+
+
+
+
+    handleLog(
+        description: string,
+        error: any
+    ){
+        console.log(`[ERROR] - ${description} - user.service.ts`);
+        console.log(error.message);
+    }
+}
